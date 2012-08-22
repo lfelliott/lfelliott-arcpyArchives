56e6ca03e73fa4ebefc9cb82e6f2e06d87b9a36d
lfelliott <lfelliott@hotmail.com>
2012-08-22 12:06:38 -0500

    Initial commit to scan system descriptions for scientific names from database.

---
+++ scinames.py
@@ -0,0 +1,28 @@
+import csv
+import re
+
+source = open("scinames.txt", "r")
+
+outfile = open("scinamelist2.txt", "a")
+outlines = []
+for line in source:
+	line2 = line[:-1]
+	species = line2 + " <(.+?)>"
+	#print line2
+	p1 = re.compile(species)
+	source2 = open("descript2.txt", "r")
+	
+	for paragraph in source2:
+		m1 = p1.search(paragraph)
+		if (m1 != None):
+			cname = m1.group(1)
+			#print species
+			#print line2
+			#print cname
+			outline = line2 + "|" + cname + "\n"
+			outfile.write(outline)
+	source2.close()
+source.close()
+outfile.close()
+
+		
