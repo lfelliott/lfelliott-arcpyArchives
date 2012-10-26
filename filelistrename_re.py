54b5aa5df03f739cd7f2992f11cafff5e068c681
lfelliott <lfelliott@hotmail.com>
2012-10-26 10:09:26 -0500

    Rename a list of mapunit_00.dbf files to mu_00.dbf

---
+++ filelistrename_re.py
@@ -0,0 +1,13 @@
+import os
+import re
+currentdir = os.getcwd()
+searchstr = re.compile(r'mapunit_(\d{2}).dbf$')
+filelist = []
+for filename in os.listdir(currentdir):
+	m1 = searchstr.match(filename)
+	if m1:
+		p1 = m1.group(1)
+		newname = "mu" + str(p1) + ".dbf"
+		print filename + " > " + newname
+		os.rename(filename, newname)
+	
