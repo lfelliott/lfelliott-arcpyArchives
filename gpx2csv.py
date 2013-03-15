deeaf03599856e5fb62189cae695139e622c468c
Lee Elliott <lfelliott@hotmail.com>
2013-03-15 08:51:30 -0500

    Added cycling through all gpx files in directory.

--- gpx2csv.py
+++ gpx2csv.py
@@ -2,25 +2,32 @@
 # Lee Elliott 13 mar 2013
 
 import re
-basefilename = "Waypoints_08-JUN-12."
-infilename = basefilename + "gpx"
-outfilename = basefilename + "csv"
-print infilename
-infile = open(infilename, "r")
+import os
+
+outfilename = "nps_p2_wp.csv"
+filelist = []
+for filename in os.listdir("."):
+	if (filename[-3:].upper() == "GPX"):
+		filelist.append(filename)
+
 outfile = open(outfilename, "a")
-ptlines = []
 regex = re.compile(r'<wpt (.+?)</wpt>')
 regex2 = re.compile(r'^lat="([0-9\.\-]+)" lon="(\S+)".+<time>(.+)</time><name>(.+)</name')
-outfile.write("\"id\", \"time\", \"lat\", \"long\"\n")
-while 1:
-	line = infile.readline()
-	if not line: break
-	ptlines = regex.findall(line)
-	for item in ptlines:
-		m1 = regex2.search(item)
-		print item
-		if m1:
-			outstr = "\"%s\", \"%s\", %s, %s\n" % (m1.group(4), m1.group(3), m1.group(1), m1.group(2))
-			outfile.write(outstr)
-infile.close()
+outfile.write("\"id\", \"time\", \"fname\", \"lat\", \"long\"\n")
+
+for fname in filelist:
+	print fname
+	infile = open(fname, "r")
+	ptlines = []
+	while 1:
+		line = infile.readline()
+		if not line: break
+		ptlines = regex.findall(line)
+		for item in ptlines:
+			m1 = regex2.search(item)
+			print item
+			if m1:
+				outstr = "\"%s\", \"%s\",\"%s\", %s, %s\n" % (m1.group(4), m1.group(3), fname, m1.group(1), m1.group(2))
+				outfile.write(outstr)
+	infile.close()	
 outfile.close()
