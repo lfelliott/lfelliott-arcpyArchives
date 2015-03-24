c8f64d3181c5eb6608b8e9f9b0d5c873360462d0
Lee Elliott <lfelliott@hotmail.com>
2015-03-24 16:19:39 -0500

    Rename files with spaces and commas in the name.

---
+++ filerename.py
@@ -0,0 +1,16 @@
+import os
+currentdir = os.getcwd()
+outfile = open('rename_list.csv', 'a')
+print currentdir
+filelist = []
+for filename in os.listdir(currentdir):
+	if (filename[-3:].upper() == "JPG"):
+		filelist.append(filename)
+outfile.write("oldname, newname\n")
+for fname in filelist:
+	newname = fname.replace('May 1', 'May1')
+	newname = newname.replace(', 2', '_2')
+	newname = newname.replace(' ', '_')
+	os.rename(fname, newname)	
+	outfile.write("\"%s\", %s\n" % (fname, newname))
+	
