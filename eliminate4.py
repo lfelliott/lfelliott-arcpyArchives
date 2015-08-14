66bbece9d9a9f6ef3786eeca6fdf5b4d106d77ed
Lee Elliott <lfelliott@hotmail.com>
2015-08-14 08:21:16 -0500

    Modified to read attributes from ID field to run loop.

--- eliminate4.py
+++ eliminate4.py
@@ -5,10 +5,18 @@ import arcpy
 from arcpy import management as DM
 arcpy.env.workspace = "F:/NPS_VegMap/PAIS/GIS/New/PAIS_Veg_Final.gdb"
 
+output_fc = "pais_veg_smthtopo_elm15e"
+preliminary_fc = "pais_veg_smthtopo_elm15d"
 tile_fc = "tiles"
 tiles = "tiles"
 DM.MakeFeatureLayer(tile_fc, tiles)
-# Local variables:
+
+# Get values of attribute ID in tiles
+rows = arcpy.SearchCursor(tile_fc, "", "", "ID")
+ids = []
+for row in rows:
+	ids.append(row.ID)
+
 def elapsed_time(t0):
 	seconds = int(round(time() - t0))
 	h,rsecs = divmod(seconds,3600)
@@ -17,16 +25,17 @@ def elapsed_time(t0):
 
 #i = 0
 #if (i == 0):
-DM.CopyFeatures("pais_veg_smthtopo_elem15b", "pais_temp1")	
+DM.CopyFeatures(preliminary_fc, "pais_temp1")	
 starttime = time()
-for i in range(0, 15):
+for id in ids:
 	processtime = time()
-	sqlstr = "ID = %d" % (i + 1)
+	sqlstr = "ID = %d" % (id)
 	in_fc = "pais_temp1"
 	elim_in = "input"
 	elim_out = "pais_temp2"
 	DM.MakeFeatureLayer(in_fc, elim_in)
-	DM.SelectLayerByAttribute(elim_in, "NEW_SELECTION", "(Class = 'X5' or Class = 'X6' or Class = 'X15') and shape_area < 1000")
+#	DM.SelectLayerByAttribute(elim_in, "NEW_SELECTION", "(Class = 'X5' or Class = 'X6' or Class = 'X15') and shape_area < 2000")
+	DM.SelectLayerByAttribute(elim_in, "NEW_SELECTION", "(Class = 'X4' or Class = 'X5' or Class = 'X6' or Class = 'X12' or Class = 'X13' or Class = 'X16' or Class = 'X17' or Class = 'X15') and shape_area < 2500")
 	print sqlstr
 	DM.SelectLayerByAttribute(tiles, "NEW_SELECTION", sqlstr)
 	DM.SelectLayerByLocation(elim_in, "INTERSECT", tiles, "", "SUBSET_SELECTION")
@@ -37,5 +46,8 @@ for i in range(0, 15):
 	DM.Delete(elim_out)
 	print elapsed_time(processtime)
 	print "Done with %d of 15" % (i + 1)
-	print elapsed_time(starttime)
+	
+print elapsed_time(starttime)
+DM.CopyFeatures("pais_temp1", output_fc)
+DM.Delete("pais_temp1")
 
